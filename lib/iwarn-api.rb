3a1e2b84844cf92cefa92bc04c10d7569ed1554f
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-05-31 17:06:17 -0500

    Load libraries and files in main file

--- lib/iwarn-api.rb
+++ lib/iwarn-api.rb
@@ -1,2 +1,14 @@
+require "nancy/base"
+require "sequel"
+
+# Main app
 require "iwarn-api/app"
-require "iwarn-api/config"
+
+# Models
+require "iwarn-api/models/event"
+
+# Controllers
+require "iwarn-api/controllers/events_controller"
+
+# Serializers
+require "iwarn-api/controllers/events_serializer"
