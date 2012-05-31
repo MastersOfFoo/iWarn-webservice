e19ac1f9d3c8e6c409c61882acb87f843a92c2a5
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-05-31 17:05:58 -0500

    Add rack/protection to API

--- lib/iwarn-api/app.rb
+++ lib/iwarn-api/app.rb
@@ -1,28 +1,9 @@
-require "nancy/base"
 class IWarnApi < Nancy::Base
+  use Rack::Protection
+  use Rack::Protection::RemoteReferrer
+
   # TODO: Provide useful info here
   get "/" do
     "iWarn API v1"
-  end
-
-  # Create a new Event with parameters pased in body
-  post "/events" do
-    event_attrs = params["event"]
-  end
-
-  # Read all existing events from database
-  get "/events" do
-
-  end
-
-  # Read the event with the given id
-  get "/events/:id" do
-    id = params["id"]
-  end
-
-  # Update an existing event with the given id
-  put "/events/:id" do
-    id = params["id"]
-    event_attrs = params["event"]
   end
 end
