e815ab916ef314924b2efc835d706d029dd0178e
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-06-02 01:44:49 -0500

    add .json to events routes

--- lib/iwarn-api/controllers/events_controller.rb
+++ lib/iwarn-api/controllers/events_controller.rb
@@ -1,21 +1,25 @@
 class IWarnApi
   # Create a new Event with parameters pased in body
-  post "/events" do
+  post "/events.json" do
     event_attrs = params["event"]
   end
 
   # Read all existing events from database
-  get "/events" do
+  get "/events.json" do
 
   end
 
   # Read the event with the given id
-  get "/events/:id" do
+  get "/events/:id.json" do
     id = params["id"]
+    event = Event[id]
+    response["Content-Type"] = "application/json"
+    halt 404, "not found" unless event
+    EventSerializer.new(event).to_json
   end
 
   # Update an existing event with the given id
-  put "/events/:id" do
+  put "/events/:id.json" do
     id = params["id"]
     event_attrs = params["event"]
   end
