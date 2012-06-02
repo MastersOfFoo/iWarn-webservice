0b1e7592df2440af80bdbf7aa2c92d1f9a826a2a
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-06-02 01:44:25 -0500

    Add EventSerializer

--- lib/iwarn-api/serializers/event_serializer.rb
+++ lib/iwarn-api/serializers/event_serializer.rb
@@ -1,2 +1,19 @@
 class EventSerializer
+  def initialize(event)
+    @event = event
+  end
+
+  def to_json(include_root = true)
+    event_hash = {
+      'description' => @event.description,
+      'latitude' => @event.latitude,
+      'longitude' => @event.longitude,
+      'state' => @event.state,
+      'type' => @event.type,
+      'created_at' => @event.created_at,
+      'update_at' => @event.updated_at,
+    }
+    event_hash = { 'event' => event_hash } if include_root
+    Yajl::Encoder.encode(event_hash)
+  end
 end
