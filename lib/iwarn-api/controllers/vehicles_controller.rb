a93b7741af3d6c17829278e518c3c7d92ad3164e
Danilo Garcia <danilogarcia024@gmail.com>
2012-06-15 07:46:40 -0500

    Vehicle Controllers Added

--- lib/iwarn-api/controllers/vehicles_controller.rb
+++ lib/iwarn-api/controllers/vehicles_controller.rb
@@ -1,33 +1,27 @@
 class IWarnApi
-  # Upload a photo to an event
-  post "/vehicles/:id/vehicles.json" do
-    event = Event[params["id"]]
-    response["Content-Type"] = "application/json"
-    halt 404, "not found" unless event
-     #We have to decided if receive just one Vehicle id or varios in json variable
-    
-    vehicle = Vehicle.create(identification_number: , event: event)   
-    #uploader = PhotoUploader.new(params["uploaded"])
-    #uploader.upload!
-    #photo = Photo.create(url: uploader.url, event: event)
+  # Associate people to an event
+  post "/vehicles/:id/vehicle.json" do
+    begin
+      event = Event[params["id"]]
+      response["Content-Type"] = "application/json"
+      halt 404, "not found" unless event
 
-    if vehicle.save
-      headers = {"Location" => photo_url(event, photo)}
-      halt 201, headers, PhotoSerializer.new(photo).to_json
-    else
+      params["vehicle"].each do |p|
+        vehicle = Vehicle.new(event: event, person_id: p)
+        vehicle.save
+      end
+
+      halt 201, headers, ArraySerializer.new(event.vehicles).to_json
+    rescue
       halt 400, "Bad Request"
     end
   end
 
-  # Get all the photos of an event
-  get "/vehicles/:id/photos.json" do
+  # Get all the people associated to an event
+  get "/vehicles/:id/vehicle.json" do
     event = Event[params["id"]]
     response["Content-Type"] = "application/json"
     halt 404, "not found" unless event
     ArraySerializer.new(event.vehicles).to_json
-  end
-
-  def photo_url(event, photo)
-    "#{request.scheme}://#{request.host}/events/#{event.id}/photos/#{photo.id}"
   end
 end
