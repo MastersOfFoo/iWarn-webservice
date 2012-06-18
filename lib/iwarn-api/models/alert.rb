bddbc70db2bb32255b64327d6cb8194e9ff8a094
Guillermo Iguaran <guilleiguaran@gmail.com>
2012-06-18 02:05:41 -0500

    Add SMS messaging in Alert#after_create callback

--- lib/iwarn-api/models/alert.rb
+++ lib/iwarn-api/models/alert.rb
@@ -2,8 +2,14 @@ class Alert < Sequel::Model
   plugin :timestamps
   plugin :validation_helpers
   many_to_one :event
-  
+  one_to_one :service
+
   def validate
-    validates_presence [:event_id]
+    validates_presence [:event_id, :service_id, :message]
+  end
+
+  def after_create
+    super
+    SmsService.new(self.service.number, self.message).deliver
   end
 end
