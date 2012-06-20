71cac6b40477a176de8f643abb4ab13c3cc8d484
Danilo Garcia <danilogarcia024@gmail.com>
2012-06-20 02:04:42 -0500

    Address Name Showing when sending Sms's

--- lib/iwarn-api/controllers/alerts_controller.rb
+++ lib/iwarn-api/controllers/alerts_controller.rb
@@ -1,20 +1,19 @@
 class IWarnApi
   # Create an alert of an Event of the given id
   post "/events/:id/alerts.json" do
-    #begin
-      p params      
+    begin
       event = Event[params["id"]]
       service = Service[params["alert"]["service_id"]]
       response["Content-Type"] = "application/json"
       halt 404, "not found" unless event || service
       
-      alert = Alert.new(event: event, service: service, message: params["alert"]["message"])
+      alert = Alert.new(event: event, service: service, message: params["alert"]["message"]+" "+event.address)
       alert.save
       headers = {"Location" => alert_url(alert, event)}
       halt 201, headers, AlertSerializer.new(alert).to_json
-    #rescue
+    rescue
       halt 400, "Bad Request"
-    #end
+    end
   end
 
   # Get all the alerts from the given id of the event
